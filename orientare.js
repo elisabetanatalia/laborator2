c31795c96d3ca01e7c7c3e3e7054635caa324ade
elisabetanatalia <72567224+elisabetanatalia@users.noreply.github.com>
2020-10-15 17:39:26 +0300

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -14,11 +14,10 @@ function on_device_orientation(evt)
 	var centru = {x:canvas.width/2, y:canvas.height/2}
 	var latura_patrat = 50;
 	
-	context.setTrandform(1, 0, 0, 1, 0, 0);
-	context.clearRect(0,0,canvas.width, canvas.height);
-	context.translate(centru.x/2, centru.y/2);
-	context.rotate(alpha * Math.PI /180);
+	
+	context.clearRect(0,0,c.width, c.height);
+	context.fillStyle = "#6A0888";
 	context.beginPath();
-	context.rect(-latura_patrat/2,-latura_patrat/2,latura_patrat, latura_patrat);
-	context.stroke();
+	context.fillRect(180, 150, event.beta, 50);
+	
 }
